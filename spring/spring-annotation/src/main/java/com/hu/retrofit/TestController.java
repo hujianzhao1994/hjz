36aea8776813570e1eda2f9c90223d4e110b8f40
hujianzhao <a123456>
2021-08-11 11:17:05 +0800

    @PropertySource 和 @ConfigurationProperties

--- spring/spring-annotation/src/main/java/com/hu/retrofit/TestController.java
+++ spring/spring-annotation/src/main/java/com/hu/retrofit/TestController.java
@@ -1,8 +1,11 @@
 package com.hu.retrofit;
 
+import com.hu.annotation.config.GookerProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * @Description TODO
@@ -16,19 +19,31 @@ public class TestController {
     @Autowired
     TestService testService;
 
+    @Resource
+    private GookerProperties gookerProperties;
+
+
     @GetMapping("/api1")
-    public Object api1(){
+    public Object api1() {
         return testService.listAll();
     }
 
 
     @GetMapping("/api2")
-    public Object api2(){
+    public Object api2() {
         return testService.listAll();
     }
 
     @GetMapping("/retrofit")
-    public Object retrofit(){
+    public Object retrofit() {
         return testService.retrofit();
     }
+
+    @GetMapping("/test")
+    public void test() {
+        System.out.println(gookerProperties.getCompanyName());
+        System.out.println(gookerProperties.getPersonNum());
+    }
+
+
 }
