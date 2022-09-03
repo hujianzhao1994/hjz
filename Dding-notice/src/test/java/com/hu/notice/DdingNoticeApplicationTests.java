d64d074f87e9c93dd950867ad676563ccbb7cca2
hujianzhao <hujianzhao@gooker.net>
2022-09-04 00:21:09 +0800

    添加钉钉报警

---
+++ Dding-notice/src/test/java/com/hu/notice/DdingNoticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hu.notice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdingNoticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
